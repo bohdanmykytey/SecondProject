82d90352880b368bcc8b955bbe5de04edfbcdd5b
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2019-08-07 13:43:10 -0400

    upfate route fixex

---
+++ controllers/venueController.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const router = express.Router();
+const Venue = require('../models/schema.js')
+
+
+// Display Routes for maind index.ejs
+router.get('/venues', (req,res) => {
+    Venue.find({}, (err, allVenues) =>  {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('image: ',allVenues.image)
+            res.render('index.ejs', {
+            venue: allVenues
+            })
+        }
+    })
+})
+
+//render add.ejs page
+router.get('/venues/new', (req, res) => {
+    res.render('add.ejs')
+})
+  
+
+//view details route
+router.get('/venues/:id', (req, res)  =>  {
+    Venue.findById(req.params.id, (err, foundVenue) =>  {
+      if (err) {
+        console.log(err)
+    } else {
+        console.log(foundVenue)
+        res.render('details.ejs', {
+        venue: foundVenue
+    })
+    }
+    })
+})
+  
+//create new route
+router.post('/venues', (req, res) => { 
+    Venue.create(req.body, (err, createdVenue)=>{
+        if (err) {
+            console.log(err)
+        } else {
+          res.redirect('/');
+        }
+    });
+});
+  
+
+//delete route
+router.delete('/venues/:id', (req, res) => {
+    Venue.findByIdAndRemove(req.params.id, (err, deletedVenue) => {
+      if (err) {
+        console.log(err)
+        } else {
+        res.redirect('/');
+        }
+    })
+});
+  
+//edit form show
+router.get('/venues/:id/edit', (req, res)=>{
+    Venue.findById(req.params.id, (err, foundVenue) => {
+      console.log('foundVenue: ', foundVenue)
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('edit.ejs', {
+                venue: foundVenue 
+            });
+        }
+    });
+});
+  
+  //edit route
+router.put('/venues/:id', (req, res) => { 
+    Venue.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedVenue)=>{
+      console.log('updated venue: ',req.body)
+        if (err) {
+            console.log(err)
+        } else {
+          res.redirect('/');
+        }
+    });
+});
+  
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/venues'
 const Venue = require('../project_2/models/schema.js')
 const dataSeed = require('./models/dataSeed.js')
 const methodOverride = require('method-override');
+const serverController = require('../project_2/controllers/venueController.js')
 
 //middleware
 app.use(methodOverride('_method'));
@@ -18,94 +19,12 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, () => {
 
 //seed route
 app.use('/seed', dataSeed)
+app.use('/', serverController)
 
 // Display Routes for maind index.ejs
 app.get('/', (req,res) => {
-  Venue.find({}, (err, allVenues) =>  {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log('image: ',allVenues.image)
-      res.render('index.ejs', {
-        venue: allVenues
-    })
-    }
-  })
+ res.redirect('/venues')
 })
 
-//view details route
-app.get('/details/:id', (req, res)  =>  {
-  Venue.findById(req.params.id, (err, foundVenue) =>  {
-    if (err) {
-      console.log(err)
-    } else {
-    console.log(foundVenue)
-    res.render('details.ejs', {
-      venue: foundVenue
-    })
-  }
-  
-  })
-})
-
-//render add.ejs page
-app.get('/venues/new', (req, res) => {
-  res.render('add.ejs')
-})
-
-//create new route
-app.post('/', (req, res) => { 
-  Venue.create(req.body, (err, createdVenue)=>{
-      if (err) {
-          console.log(err)
-      } else {
-        res.redirect('/');
-      }
-  });
-});
-
-//delete route
-app.delete('/details/:id', (req, res) => {
-    Venue.findByIdAndRemove(req.params.id, (err, deletedVenue) => {
-      if (err) {
-        console.log(err)
-      } else {
-    res.redirect('/');
-    }
-  })
-});
-
-app.get('/details/:id/edit', (req, res)=>{
-  Venue.findById(req.params.id, (err, foundVenue) => {
-    console.log('foundVenue: ', foundVenue)
-      if (err) {
-          console.log(err)
-      } else {
-          res.render('edit.ejs', {
-              venue: foundVenue 
-          });
-      }
-  });
-});
-
-//edit route
-app.put('/details/:id', (req, res) => { 
-  Venue.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedVenue)=>{
-    console.log('updated venue: ',req.body)
-      if (err) {
-          console.log(err)
-      } else {
-        res.redirect('/');
-      }
-  });
-});
-
-
-
-
-
-app.get('/' , (req, res) => {
-  res.send('app is running!');
-});
 
 app.listen(PORT, () => console.log('Listening on port:', PORT));
